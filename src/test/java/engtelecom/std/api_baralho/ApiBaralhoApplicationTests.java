431b7f7c7612249b3c70010971f439cc6edc4032
Lucas Costa Fontes <luka1.fontes@gmail.com>
2024-08-05 14:45:31 -0300

    Criando projeto spring e lombok

---
+++ src/test/java/engtelecom/std/api_baralho/ApiBaralhoApplicationTests.java
@@ -0,0 +1,13 @@
+package engtelecom.std.api_baralho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBaralhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
